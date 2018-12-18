94a2da20ae918786e3e58c8b88dc24f97d1a55a6
vinay_code5 <vinay@code5.org>
2018-12-18 18:44:24 +0530

    Readme updated

--- examples/lambda/Categories/index.js
+++ examples/lambda/Categories/index.js
@@ -3,5 +3,11 @@ console.log("okasdas");
 exports.handler = function(event,context,callback){
     console.log("Executed");
     console.log("I wanna funk baby");
-    callback("fuck bitches");
+    callback(null,{
+        "statusCode":300,
+        "headers":{
+            "x-amz":"age=121"
+        },
+        "body":"fuck bitches"
+    });
 }
